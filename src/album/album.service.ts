86a3d1607b74844fdf0ee58ee3d38f0007ed27c0
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2023-11-30 10:46:43 -0500

    FEAT: added logic to service methods

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -14,11 +14,23 @@ export class AlbumService {
         private readonly albumRepository: Repository<AlbumEntity>
     ){}
 
-    async findOne(id: number): Promise<AlbumEntity> {
-        const museum: AlbumEntity = await this.albumRepository.findOne({where: {id: id,}, relations: ["fotos",] } );
-        if (!museum)
+    async findAlbumById(id: number): Promise<AlbumEntity> {
+        const album: AlbumEntity = await this.albumRepository.findOne({where: {id: id,}, relations: ["fotos",] } );
+        if (!album)
           throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
    
-        return museum;
+        return album;
+    }
+
+    async createAlbum(album: AlbumEntity): Promise<AlbumEntity> {
+        return await this.albumRepository.save(album);
+    }
+
+    async deleteAlbum(id: number) {
+        const album: AlbumEntity = await this.albumRepository.findOne({where:{id}});
+        if (!album)
+          throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
+     
+        await this.albumRepository.remove(album);
     }
 }
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -2,6 +2,8 @@ import { InjectRepository, TypeOrmModule } from "@nestjs/typeorm";
 import { UsuarioEntity } from "./usuario.entity/usuario.entity";
 import { Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
+import { BusinessError, BusinessLogicException } from "src/shared/errors/business-errors";
+import { AlbumEntity } from "src/album/album.entity/album.entity";
 
 imports: [TypeOrmModule.forFeature([UsuarioEntity])]
 
@@ -15,4 +17,17 @@ export class UsuarioService {
     async findAllUsuarios(): Promise<UsuarioEntity[]> {
         return await this.usuarioRepository.find({ relations: ["fotos",] });
     }
+
+    async findUsuarioById(id: number): Promise<UsuarioEntity> {
+        const museum: UsuarioEntity = await this.usuarioRepository.findOne({where: {id: id,}, relations: ["redsocial",] } );
+        if (!museum)
+          throw new BusinessLogicException("The album with the given id was not found", BusinessError.NOT_FOUND);
+   
+        return museum;
+    }
+
+    async createUsuario(album: UsuarioEntity): Promise<UsuarioEntity> {
+        return await this.usuarioRepository.save(album);
+    }
+
 }
