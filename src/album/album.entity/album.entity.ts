c1f991663dc4ef381e09e5c5ba53b391c822c941
Diana Alejandra Silva Alvarez <da.silvaa@uniandes.edu.co>
2023-11-30 10:12:15 -0500

    added album entity and foto entity and its first relations

---
+++ src/album/album.entity/album.entity.ts
@@ -0,0 +1,17 @@
+import { FotoEntity } from 'src/foto/foto.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AlbumEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: number;
+
+ @Column()
+ fechaInicio: Date;
+ 
+ @Column()
+ fechaFin: Date;
+
+ @OneToMany(() => FotoEntity, foto => foto.album)
+   fotos: FotoEntity[];
+}
--- src/foto/foto.entity.ts
+++ src/foto/foto.entity.ts
@@ -1,1 +1,23 @@
-export class FotoEntity {}
+import { AlbumEntity } from 'src/album/album.entity/album.entity';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class FotoEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: number;
+
+ @Column()
+ ISO: number;
+ 
+ @Column()
+ velObturacion: number;
+
+ @Column()
+ apertura: number;
+
+ @Column()
+ fecha: Date;
+
+ @ManyToOne(() => AlbumEntity, album => album.fotos)
+ album: AlbumEntity;
+}
